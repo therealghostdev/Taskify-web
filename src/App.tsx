aea5a95da1e40ebc59a58df8fea0e93d107a19b0
DaScOrPiO <dunni82@yahoo.com>
2024-05-08 15:27:03 +0100

    FEAT: app routes and protection

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,89 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 import Homepage from "./pages/homepage";
 import Nav from "./components/general/navigation";
 import Auth from "./pages/auth";
 import Calendar from "./pages/calendar";
 import Focus from "./pages/focus_page";
 import Profile from "./pages/profile";
+import { useThemeContext, useAuthContext } from "./utils/app_context/general";
+import { useEffect } from "react";
+import { ProtectedRouteProps } from "./utils/types/todo";
 
 function App() {
+  const { darkMode, toggleDarkMode } = useThemeContext();
+  const authenticated = useAuthContext();
+
+  // useEffect(() => {
+  //   localStorage.setItem("token", "hello");
+  // }, [authenticated]);
+
   return (
-    <>
+    <main
+      className={`${
+        darkMode ? "dark-theme" : "light-theme"
+      } w-screen min-h-screen`}
+    >
       <Router>
-        <Nav />
+        {authenticated && <Nav />}
         <Routes>
-          <Route element={<Auth />} index path="/auth" />
-          <Route element={<Homepage />} path="/" />
-          <Route element={<Calendar />} path="/calendar" />
-          <Route element={<Focus />} path="/focus" />
-          <Route element={<Profile />} path="/page3" />
-          {/* <Route element={<Page4 />} path="/page4" /> */}
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute authenticated={authenticated} redirectTo="/auth">
+                <Homepage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/calendar"
+            element={
+              <ProtectedRoute authenticated={authenticated} redirectTo="/auth">
+                <Calendar />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/focus"
+            element={
+              <ProtectedRoute authenticated={authenticated} redirectTo="/auth">
+                <Focus />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/page3"
+            element={
+              <ProtectedRoute authenticated={authenticated} redirectTo="/auth">
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/auth" element={<Auth />} />
         </Routes>
       </Router>
-    </>
+    </main>
   );
+}
+
+function ProtectedRoute({
+  authenticated,
+  redirectTo,
+  children,
+}: ProtectedRouteProps) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!authenticated) {
+      navigate(redirectTo);
+    }
+  }, [authenticated, redirectTo, navigate]);
+
+  return authenticated ? <>{children}</> : null;
 }
 
 export default App;
