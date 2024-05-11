28b59ecc1598036f094600c30dbd5e595448127d
DaScOrPiO <dunni82@yahoo.com>
2024-05-11 01:40:30 +0100

    FEAT: added click outside for task-flow conntainer

--- src/components/general/add-task/index.tsx
+++ src/components/general/add-task/index.tsx
@@ -4,24 +4,46 @@ import {
 } from "../../../utils/app_context/general";
 import AddTaskName from "./AddTaskName";
 import AddDate from "./AddDate";
+import { useEffect, useRef } from "react";
 
 export default function AddTask() {
   const { todos } = useTodoContext();
-  const { trackScreen } = useTrackContext();
+  const { trackScreen, trackScreenFunc } = useTrackContext();
+  const flowContainerRef = useRef<HTMLDivElement>(null);
   console.log(todos, trackScreen);
 
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        flowContainerRef.current &&
+        !flowContainerRef.current.contains(e.target as Node)
+      ) {
+        trackScreenFunc("");
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
   return (
-    <div
-      className={`fixed flex justify-center items-center p-4 lg:w-2/4 md:w-3/4 w-full lg:top-16 md:top-52 top-36 left-0 h-auto lg:translate-x-1/2 
-  md:translate-x-20 bg-transparent`}
-    >
-      {trackScreen === "name" ? (
-        <AddTaskName />
-      ) : trackScreen === "calendar" ? (
-        <AddDate />
-      ) : (
-        ""
-      )}
-    </div>
+    ["name", "calendar", "time", "priority", "category"].includes(
+      trackScreen
+    ) && (
+      <div
+        ref={flowContainerRef}
+        className={`fixed flex justify-center items-center p-4 lg:w-2/4 md:w-3/4 w-full lg:top-16 md:top-52 top-36 left-0 h-auto lg:translate-x-1/2 
+        md:translate-x-20 bg-transparent`}
+      >
+        {trackScreen === "name" ? (
+          <AddTaskName />
+        ) : trackScreen === "calendar" ? (
+          <AddDate />
+        ) : (
+          ""
+        )}
+      </div>
+    )
   );
 }
