52b5d259f20a8752c4a52f2f0c6824216db83004
Promise Obioma <proekene16@gmail.com>
2024-05-14 12:30:35 +0100

    update

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,9 +1,11 @@
 import Index from "../components/authentication";
+// import Signup from "../components/authentication/Signup";
 
 export default function Auth() {
   return (
     <>
       <Index />
+
     </>
   );
 }
