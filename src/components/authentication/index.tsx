48aae1a8ca42884fa4a873b513ab575ff43a07cd
Promise Obioma <proekene16@gmail.com>
2024-05-15 08:40:10 +0100

    update on the reg and login

--- src/components/authentication/index.tsx
+++ src/components/authentication/index.tsx
@@ -1,3 +1,23 @@
+import { useState } from 'react'
+import Login from './Login'
+import Register from './Register'
+
+
 export default function Index() {
-  return <div>index</div>;
+  const [isLogin, setIsLogin] = useState(true)
+   const [isRegister, setIsRegister] = useState(false)
+
+  const registerSwap = ()=>{
+    setIsLogin(!isLogin)
+  }
+  const loginSwap = ()=>{
+    setIsRegister(!isRegister)
+  }
+  return (
+    <div>
+      {isLogin ? (
+        <Login registerSwap={registerSwap} />): <Register loginSwap={loginSwap} />
+      }
+    </div>
+  )
 }
